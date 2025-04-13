160c9442ed64c681745828d0b1f47edd862db1fc
Sakshi Tak <165888889+Sakshitak19@users.noreply.github.com>
2025-04-13 17:17:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Portfolio/", // 👈 use your actual repo name here
   plugins: [react()],
-})
+});
